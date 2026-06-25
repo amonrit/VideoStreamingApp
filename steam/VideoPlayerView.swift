526c0738d09afdfa24897848e97c4de5dcddfb09
Amonrit <amonrit@Amonrits-MacBook-Air.local>
2026-06-25 03:25:55 -0700

    test AI PR review

---
+++ steam/VideoPlayerView.swift
@@ -0,0 +1,24 @@
+//
+//  VideoPlayerView.swift
+//  steam
+//
+//  Created by Amonrit on 25/6/2569 BE.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    
+    @ObservedObject var viewModel: VideoPlayerViewModel
+    
+    var body: some View {
+        VideoPlayer(player: viewModel.player)
+            .onAppear {
+                viewModel.play()
+            }
+            .onDisappear {
+                viewModel.pause()
+            }
+    }
+}
