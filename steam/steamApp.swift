41d4c102b51beaf5d051fb9798a7c9d4c542cce4
Amonrit <amonrit@Amonrits-MacBook-Air.local>
2026-06-25 06:01:27 -0700

    feature: add video steaming

---
+++ steam/steamApp.swift
@@ -0,0 +1,17 @@
+//
+//  steamApp.swift
+//  steam
+//
+//  Created by Amonrit on 25/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct steamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
